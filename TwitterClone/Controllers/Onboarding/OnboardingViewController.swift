627801ea6c3a73654599e40695310393e78d2058
Youssef0Eldeeb <youssefeldeeb3109@gmail.com>
2023-11-13 16:49:56 +0200

    Configuration of Firebase Package

---
+++ TwitterClone/Controllers/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingViewController.swift
+//  TwitterClone
+//
+//  Created by Youssef Eldeeb on 13/11/2023.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .systemBackground
+    }
+
+}
--- TwitterClone/Controllers/TabBarCoreVCs/HomeViewController.swift
+++ TwitterClone/Controllers/TabBarCoreVCs/HomeViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class HomeViewController: UIViewController {
 
@@ -29,6 +30,11 @@ class HomeViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.navigationBar.isHidden = false
+        if Auth.auth().currentUser == nil {
+            let vc = UINavigationController(rootViewController: OnboardingViewController())
+            vc.modalPresentationStyle = .fullScreen
+            present(vc, animated: false)
+        }
     }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
